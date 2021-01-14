41f8bd675cd3c3b0a7b964cd284c2a8af64c5a47
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-14 14:13:02 -0600

    add workflow and tf files

---
+++ terraform/resource_group.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = ">= 2.26 "
+        }
+    }
+}
+
+provider "azurerm" {
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "myTFResourceGroup"
+    location = "eastus"
+    tags = {
+      "Environment" = "Lab"
+    }
+}
