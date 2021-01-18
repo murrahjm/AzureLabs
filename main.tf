4c3142d6d7ae88ea93945b81c6eb0f1d31740a1b
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-18 15:16:41 -0600

    network depends on resource group

--- main.tf
+++ main.tf
@@ -30,4 +30,5 @@ module "network" {
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  depends_on          = [azurerm_resource_group.rg]
 }
