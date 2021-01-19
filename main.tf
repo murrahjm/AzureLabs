9a8910de5da4a083de08afd02545f45b631130f6
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-19 08:31:23 -0600

    vnet module not needed

--- main.tf
+++ main.tf
@@ -87,8 +87,3 @@ module "network" {
   depends_on          = [azurerm_resource_group.rg]
 }
 
-module "vnet" {
-  source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.rg.name
-  depends_on          = [azurerm_resource_group.rg]
-}
