312e1c74b99ac68b3aa96c8b67522e612869ea43
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-19 09:55:17 -0600

    put azure creds in ENV for backend config

--- main.tf
+++ main.tf
@@ -13,10 +13,6 @@ terraform {
     storage_account_name = "tfstate145343"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    subscription_id      = var.subscription_id
-    client_id            = var.client_id
-    client_secret        = var.client_secret
-    tenant_id            = var.tenant_id
   }
 }
 
