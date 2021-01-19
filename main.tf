3aa1e51e0e2d3623557a18aceac4d763c2d92427
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-19 08:12:58 -0600

    resource group reference should be variable not string

--- main.tf
+++ main.tf
@@ -89,6 +89,6 @@ module "network" {
 
 module "vnet" {
   source              = "Azure/vnet/azurerm"
-  resource_group_name = "azurerm_resource_group.rg.name"
+  resource_group_name = azurerm_resource_group.rg.name
 
 }
