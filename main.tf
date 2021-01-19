8f9268a9ba9061549ea17aa065be395865b4bad8
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-19 15:06:54 -0600

    add ip configuration

--- main.tf
+++ main.tf
@@ -116,11 +116,21 @@ resource "azurerm_network_interface" "webserver" {
   name                = "webserver-nic"
   location            = local.location
   resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.network.vnet_subnets[1]
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 resource "azurerm_network_interface" "sqlserver" {
   name                = "sqlserver-nic"
   location            = local.location
   resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.network.vnet_subnets[2]
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "webserver" {
