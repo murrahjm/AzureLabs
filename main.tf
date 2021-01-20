4a4efa1ff4f0d4cdfa22319c66c8683da29a50d3
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-20 12:00:44 -0600

    try vnet module

--- main.tf
+++ main.tf
@@ -104,12 +104,17 @@ module "network" {
   # module.network.vnet_location
   # module.network.vnet_address_space
   # module.network.vnet_subnets
-  source              = "Azure/network/azurerm"
+  source              = "Azure/vnet/azurerm"
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["central", "webservers", "sqlservers"]
   depends_on          = [azurerm_resource_group.rg]
+  nsg_ids = {
+    central    = module.network-security-group1.network_security_group_id
+    webservers = module.network-security-group2.network_security_group_id
+    sqlservers = module.network-security-group3.network_security_group_id
+  }
 }
 
 resource "azurerm_network_interface" "webserver" {
