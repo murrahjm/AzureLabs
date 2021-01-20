3bcfc2d8a8c2e7ce187dc26410026e2d34952cc0
Jeremy Murrah <jmmurrah@eprod.com>
2021-01-20 12:02:51 -0600

    address space should be a list

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ module "network" {
   # module.network.vnet_subnets
   source              = "Azure/vnet/azurerm"
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = "10.0.0.0/16"
+  address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["central", "webservers", "sqlservers"]
   depends_on          = [azurerm_resource_group.rg]
